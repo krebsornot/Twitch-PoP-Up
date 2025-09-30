5f0a3b3541f1e9a68fa2ba16dd150bf8e5aceee0
krebsornot <115174393+krebsornot@users.noreply.github.com>
2025-09-30 23:07:47 +0200

    Update before Sleep

--- Desktop/pop up/script.js
+++ Desktop/pop up/script.js
@@ -2,21 +2,6 @@ window.onload = () => {
   const alertBox = document.getElementById('alertBox');
   const sound = document.getElementById('sound');
   const nameField = document.getElementById('name');
-  
-  // EventSource für Server
-const evtSource = new EventSource('http://localhost:3000/events');
-
-evtSource.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    console.log('Event empfangen:', data);
-
-    // Popup Text anpassen
-    nameField.textContent = data.user;
-    alertBox.style.opacity = '1';
-    alertBox.style.transform = 'translateX(-50%) translateY(0) scale(1)';
-
-    // Sound abspielen
-    sound.play();
 
   // Testname (kann später dynamisch ersetzt werden)
   nameField.textContent = "Maya";
